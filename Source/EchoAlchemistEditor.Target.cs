875d4928e025bc4a7bb88ee45f8080992046d4a3
gdszyy <41259084+gdszyy@users.noreply.github.com>
2025-12-14 13:54:24 +0800

    修改了ini配置

--- Source/EchoAlchemistEditor.Target.cs
+++ Source/EchoAlchemistEditor.Target.cs
@@ -8,7 +8,7 @@ public class EchoAlchemistEditorTarget : TargetRules
 	public EchoAlchemistEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 		ExtraModuleNames.AddRange( new string[] { "EchoAlchemist" } );
 	}
 }
